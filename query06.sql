7d726ccc623ea6f8aef2ff238249095ae4e6d344
Bringeroflagg <47066810+Bringeroflagg@users.noreply.github.com>
2023-02-26 20:18:24 -0800

    Update query06.sql

--- query06.sql
+++ query06.sql
@@ -1,8 +1,5 @@
 -- Retrieve the display name of all users who have
 -- posted at least one post, ordered ascending
--- 1.1 marks: <5 operators
--- 1.0 marks: <7 operators
--- 0.8 marks: correct answer
 
 SELECT DISTINCT `DisplayName`
 FROM `User`
