2f11d5259ec80214ce8aa11411603dca2c1371af
Bringeroflagg <47066810+Bringeroflagg@users.noreply.github.com>
2023-02-26 20:18:14 -0800

    Update query05.sql

--- query05.sql
+++ query05.sql
@@ -1,9 +1,6 @@
 -- Retrieve by name and frequency and ordered by increasing name
 -- those unique Badges that have been awarded at least five times,
 -- but never before 2014 and never after 2019
--- 1.1 marks: <6 operators
--- 1.0 marks: <8 operators
--- 0.8 marks: correct answer
 
 SELECT `Name`, COUNT(*) AS `Frequency`
 FROM Badge
