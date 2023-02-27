85547944aa08c6d3076eed97270057fcaf8f144b
Bringeroflagg <47066810+Bringeroflagg@users.noreply.github.com>
2023-02-26 20:17:54 -0800

    Update query02.sql

--- query02.sql
+++ query02.sql
@@ -2,9 +2,6 @@
 -- user with ID 3 awarded after 2011, together with the
 -- first date after 2011 in which that user obtained that badge.
 -- Order the results by increasing Badge Name.
--- 1.1 marks: <6 operators
--- 1.0 marks: <7 operators
--- 0.8 marks: correct answer
 
 SELECT `Name`, MIN(`Date`) AS `Date`
 FROM `Badge`
